06a197907a60654e532e51ccc28a5c66577ab7a2
Arbuzzzi <safonov.mir@gmail.com>
2019-11-15 16:07:48 +0300

    add dist

---
+++ dist/precache-manifest.e5f42e1c1c962ed99ce78f4c01b69b31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856dcf6519eb0c832cb6",
+    "url": "css/app.1f85a81b.css"
+  },
+  {
+    "revision": "e7709b068cb25a32169e",
+    "url": "css/chunk-e10d4038.747cbd80.css"
+  },
+  {
+    "revision": "06e3254763c9878aad29",
+    "url": "css/chunk-vendors.2346642d.css"
+  },
+  {
+    "revision": "bc09632c1d90bc117ed928ce2b8f8c4e",
+    "url": "index.html"
+  },
+  {
+    "revision": "856dcf6519eb0c832cb6",
+    "url": "js/app.d564b4a7.js"
+  },
+  {
+    "revision": "e7709b068cb25a32169e",
+    "url": "js/chunk-e10d4038.38159885.js"
+  },
+  {
+    "revision": "06e3254763c9878aad29",
+    "url": "js/chunk-vendors.e3fedcfc.js"
+  },
+  {
+    "revision": "736af7581a93d3e0a83f3cad8aecb223",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
